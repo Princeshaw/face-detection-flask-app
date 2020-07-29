de5806e27714f526c8b3e303a0ffb64cae5db32c
Princeshaw <prince54shaw@gmail.com>
2020-07-30 03:19:28 +0530

    Created a flask upload function and sending it to model

--- flask-app/app/views.py
+++ flask-app/app/views.py
@@ -1,5 +1,8 @@
 from flask import render_template, request
 from flask import redirect, url_for
+import os
+from PIL import Image
+UPLOAD_FOLDER= 'static/uploads'
 
 def base():
     return render_template('base.html')
@@ -8,4 +11,21 @@ def index():
     return render_template('index.html')
 
 def faceApp():
-    return render_template('faceapp.html')    
+    return render_template('faceapp.html') 
+
+def getWidth(path):
+    img = Image.open(path)
+    size = img.size #width and height
+    aspect = size[0]/size[1]
+    w = 300*aspect
+    return w
+
+def gender():
+    if request.method=='POST':
+        f = request.files['image']
+        filename = f.filename
+        path = os.path.join(UPLOAD_FOLDER,filename)
+        f.save(path)
+        w = getWidth(path)
+        return render_template('gender.html',fileupload=True,image = filename,w=w)
+    return render_template('gender.html', fileupload=False)        
--- flask-app/main.py
+++ flask-app/main.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from app import views
+import os
 
+UPLOAD_FOLDER= 'static/uploads'
 app = Flask(__name__,static_url_path='/static')
 
 #url
 app.add_url_rule('/base','base',views.base)
 app.add_url_rule('/','index',views.index)
 app.add_url_rule('/faceapp','faceapp',views.faceApp)
+app.add_url_rule('/gender','gender',views.gender, methods=['GET','POST'])
 
 
 
